21f534137ad1b75b71a5636065c7f09bc73318c2
XSmile2008 <XSmile2008@gmail.com>
2015-12-09 12:43:07 +0200

    just commit

--- scheme-io.ads
+++ scheme-io.ads
@@ -4,6 +4,7 @@ package Scheme.IO is
    function readScheme (filename : in String; schemeType : TSchemeType) return TScheme;
    procedure showScheme (scheme : in TScheme);
    procedure showLifeTime (scheme : in TScheme; plan : in TPlan; lifeTime : in Float);
+   procedure writeLifeTime (scheme : in TScheme; plan : in TPlan; lifeTime : in Float; fileName : in String);
 
    function readSchemeParSeq (filename : in String; schemeType : TSchemeType) return TScheme;
    procedure showSchemeParSeq (scheme : in TScheme);
